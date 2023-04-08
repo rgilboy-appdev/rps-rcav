5080fd152fa3e268c9299924815ca145b37b1807
Ryan Gilboy <rgilboy@chicagobooth.edu>
2023-04-08 23:47:56 +0000

    Lots of hard work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,29 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+
+  def homepage
+    render({:template => "game_templates/rules.html.erb".html_safe})
+  end
+
+  def play_rock
+    #write a ton of code
+
+    render({:template => "game_templates/user_rock.html.erb".html_safe})
+  end
+
+  def play_paper
+
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({:template => "game_templates/user_paper.html.erb".html_safe})
+  end
+
 end
